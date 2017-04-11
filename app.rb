b85963efe5dade81a70aa74c6e3a44314bec0637
spartakusQ <spartakus1688@gmail.com>
2017-04-11 19:16:09 +0300

    Refactoring extrct logger to separate file

--- app.rb
+++ app.rb
@@ -1,30 +1,3 @@
-class Logger
+require './logger'
 
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
-
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-    return @@x
-  end
-
-  def self.say_something
-    puts "lala"
-  end
-
-  def log_something wat
-    @f.puts wat
-  end
-end
-
-Logger.say_something
-Logger.instance.log_something 'wablala'
-Logger.instance.log_something 'wablala2'
-
-logger = Logger.new
-logger.log_something 'boom'
+Logger.instance.log_something 'how di dudi'
---
+++ logger.rb
@@ -0,0 +1,19 @@
+class Logger
+
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  @@x = Logger.new
+
+  def self.instance
+    return @@x
+  end
+
+  def log_something wat
+    @f.puts wat
+  end
+
+  private_class_method :new
+
+end
